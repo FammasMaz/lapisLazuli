3d3ad299bf3cefd4be9db00b576f6f6d7c3ea0b2
Julian Liu <wlootlxt123@gmail.com>
2019-08-30 12:51:10 +0800

    cpufreq: schedutil: Enable iowait boost by default
    Signed-off-by: Ratoriku <a1063021545@gmail.com>
    Signed-off-by: moeezehassan <hmoeeze@gmail.com>
    Signed-off-by: Moeeze Hassan <fammas.maz@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -769,7 +769,7 @@ static int sugov_init(struct cpufreq_policy *policy)
                 }
 	}
 
-		tunables->iowait_boost_enable = false;
+	tunables->iowait_boost_enable = 1;
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
