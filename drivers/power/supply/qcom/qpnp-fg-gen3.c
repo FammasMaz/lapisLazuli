9a33b2733c43a944171526b984e6e2ee224aacab
Moeeze Hassan <fammas.maz@gmail.com>
2021-01-28 23:59:26 +0500

    Revert "qpnp: fix a redefinition"
    This reverts commit 95c568f2733995477a9c60497c91463ba8a25538.

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -1339,6 +1339,17 @@ static bool usb_psy_initialized(struct fg_chip *chip)
 	return chip->usb_psy;
 }
 
+static bool is_parallel_charger_available(struct fg_chip *chip)
+{
+	if (chip->usb_psy)
+		return true;
+
+	chip->usb_psy = power_supply_get_by_name("usb");
+	if (!chip->usb_psy)
+		return false;
+
+	return true;
+}
 
 static bool pc_port_psy_initialized(struct fg_chip *chip)
 {
