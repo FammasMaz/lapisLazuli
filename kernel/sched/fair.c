678b905322128cdf2fb49c4b8100306300d1eb8c
Moeeze Hassan <fammas.maz@gmail.com>
2022-02-28 03:13:24 +0100

    Test and fuck

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -134,7 +134,7 @@ unsigned int sysctl_sched_cfs_bandwidth_slice = 5000UL;
  * The margin used when comparing utilization with CPU capacity:
  * util * margin < capacity * 1024
  */
-unsigned int capacity_margin = 1280; /* ~20% */
+unsigned int capacity_margin = 1351; /* ~20% */
 
 static unsigned int __maybe_unused sched_small_task_threshold = 102;
 
